d69e43593eeeb2484518e81cb0e9ea1cb5280cc0
AdryDev <adrydevmateo@gmail.com>
2024-06-07 23:58:30 -0400

    Added black theme

--- src/todo/Todo.comp.tsx
+++ src/todo/Todo.comp.tsx
@@ -16,6 +16,8 @@ export default function TodoComp({ txt, complete, OnEditTodo, OnDeleteTodo }: To
    <div className="col-1">
     <button className='checkbox' type='button' onClick={() => OnEditTodo()}>
      <img src={IconCheck} alt="checkbox icon" width={10} height={10} />
+     {/* TODO: use svg */}
+     {/* <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4z" /></svg> */}
     </button>
     <div className="txt-container">
      <strong>{txt}</strong>
