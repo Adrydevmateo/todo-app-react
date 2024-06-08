058e5e1eee12b58415b791cc35bb1f0fb4cc8e35
AdryDev <adrydevmateo@gmail.com>
2024-06-08 09:08:08 -0400

    Fixed checkbox animation

--- src/todo/Todo.comp.tsx
+++ src/todo/Todo.comp.tsx
@@ -1,6 +1,5 @@
 import './Todo.style.css'
 
-import IconCheck from '../assets/icons/icon-check.svg'
 import IconDelete from '../assets/icons/icon-cross.svg'
 
 interface TodoCompProps {
@@ -15,9 +14,7 @@ export default function TodoComp({ txt, complete, OnEditTodo, OnDeleteTodo }: To
   <div className={`todo-comp ${complete ? 'complete' : ''}`}>
    <div className="col-1">
     <button className='checkbox' type='button' onClick={() => OnEditTodo()}>
-     <img src={IconCheck} alt="checkbox icon" width={10} height={10} />
-     {/* TODO: use svg */}
-     {/* <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4z" /></svg> */}
+     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4z" /></svg>
     </button>
     <div className="txt-container">
      <strong>{txt}</strong>
